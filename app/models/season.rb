71699181cff5eeba3360ea8b96650b7962f87515
Werner <werner_a_e@yahoo.es>
2016-01-14 11:58:46 -0430

    creates a habtm relationship between season, movie and purchase_option

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,3 +1,4 @@
 class Season < ActiveRecord::Base
   has_many :episodes
+  has_and_belongs_to_many :purchase_options
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114160511) do
+ActiveRecord::Schema.define(version: 20160114161028) do
 
   create_table "episodes", force: :cascade do |t|
     t.integer  "season_id"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20160114160511) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "movies_purchase_options", id: false, force: :cascade do |t|
+    t.integer  "movie_id"
+    t.integer  "purchase_option_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "movies_purchase_options", ["movie_id"], name: "index_movies_purchase_options_on_movie_id"
+  add_index "movies_purchase_options", ["purchase_option_id"], name: "index_movies_purchase_options_on_purchase_option_id"
+
   create_table "purchase_options", force: :cascade do |t|
     t.decimal  "price"
     t.string   "video_quality"
@@ -42,5 +52,15 @@ ActiveRecord::Schema.define(version: 20160114160511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "seasons_purchase_options", id: false, force: :cascade do |t|
+    t.integer  "season_id"
+    t.integer  "purchase_option_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "seasons_purchase_options", ["purchase_option_id"], name: "index_seasons_purchase_options_on_purchase_option_id"
+  add_index "seasons_purchase_options", ["season_id"], name: "index_seasons_purchase_options_on_season_id"
 
 end
